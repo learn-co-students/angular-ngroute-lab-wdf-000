fe2c839e4bc73446682828dda9d0889255f90a06
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-08-29 09:12:38 -0400

    Done.

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,20 @@
 angular
-    .module('app', ['']);
+    .module('app', ['ngRoute'])
+    .service('UserService', function($http) {
+       this.getUser = function (name) {
+	return $http.get('http://0.0.0.0:8882/rest/user/' + name);
+       }
+    })
+    .config(function($routeProvider) {
+      $routeProvider
+      // route has to be `:id`, didn't work with `:name`
+	.when('/user/:id', {
+	  templateUrl: 'views/user.html',
+	  controller: 'UserController as user',
+	  resolve: {
+	    user: function ($route, UserService) {
+	      return UserService.getUser($route.current.params.id).data;
+	    }
+	  }
+	});
+    });
