478395d9931a0b2521894f23a8a0aeb18dd39e27
blessedsoy <unmigong@gmail.com>
2016-09-20 10:48:29 -0400

    Done.

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,32 @@
 angular
-    .module('app', ['']);
+    .module('app', ['ngRoute'])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/user/:id', {
+                templateUrl: 'views/user.html',
+                controller: 'UserController',
+            // this is ran as soon as we initiate the module (ng-app)
+                resolve: {
+                  user: function ($routeParams, UserService) {
+                      return UserService.getUser($routeParams.id);
+                  }
+                }
+            });
+  });
+
+
+
+// angular
+//     .module('app', ['ngRoute'])
+//   .config(function ($routeProvider) {
+//     $routeProvider
+//       .when('/user/:id', {
+//         templateUrl: 'views/user.html',
+//         controller: 'UserController as user',
+//         resolve: {
+//           user: function ($http, $route) {
+//             return $http.get('http://0.0.0.0:8882/rest/user/' + $route.current.params.id);
+//           }
+//         }
+//       });
+//   });
